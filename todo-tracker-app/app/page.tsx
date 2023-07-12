910033f82ab72e74ed2bcad5d8334eff2db3d93c
Naveen Kairamkonda <naveenkairamkonda26@gmail.com>
2023-07-12 10:27:54 +0530

    remove header

--- todo-tracker-app/app/page.tsx
+++ todo-tracker-app/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>To Do Tracker</h2>
       <Login />
       
     </main>
